1b02734370fd3d12821e51ed847ede547e6fe337
alexanderLjungstedt <alexander.ljungstedt@outlook.com>
2017-08-02 19:43:40 +0200

    made cosmetic changes

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
-  for (var i = 0; i < 18; i++) {
+  $('body').fadeTo('slow', 1);
+
+  for (var i = 0; i < 1250; i++) {
     $('#drawing-area').append("<div class='pixel'></div>");
   }
 
   $('.pixel').mouseenter(function(){
     $(this).addClass('active');
   });
+
+
 
 });
